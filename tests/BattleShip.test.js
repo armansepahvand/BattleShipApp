aef095509cca5c19c1e9460649a83d81954da37c
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-19 14:41:54 -0700

    refactored the test for two seperate test for vertical and horizontal ships

--- tests/BattleShip.test.js
+++ tests/BattleShip.test.js
@@ -1,8 +1,12 @@
 const BattleShip = require('../classes/BattleShip');
 
-test('Ship blocks should be in sequence numerically or alphabetically', () => {
-    const battleShipVertical= new BattleShip("b3",3,"v")
-    const battleShipHorizontal= new BattleShip("b3",3,"h")
-    expect(battleShipVertical.sequence).toEqual("b3b4b5");
-    expect(battleShipHorizontal.sequence).toEqual("b3c3d3");
-})
+test('Vertical Ship blocks should be in sequence numerically ', () => {
+  const battleShipVertical = new BattleShip('b3', 3, 'v');
+  expect(battleShipVertical.sequenceOfSlots).toEqual('b3b4b5');
+});
+
+test('Ship blocks should be in sequence  alphabetically', () => {
+  const battleShipHorizontal = new BattleShip('b3', 3, 'h');
+  console.log(battleShipHorizontal.orientation);
+  expect(battleShipHorizontal.sequenceOfSlots).toEqual('b3c3d3');
+});
