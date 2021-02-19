636fc68e07809b3fc698353139d14e39ab27668f
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-19 16:12:21 -0700

    Created the test for plaayer class

---
+++ tests/Player.test.js
@@ -0,0 +1,7 @@
+const Player = require('../classes/Player');
+
+test('Player default properties shuold  match the values ', () => {});
+
+test('Check the seters and getters of the Player Class', () => {});
+
+test('Check the shots filred by player ', () => {});
