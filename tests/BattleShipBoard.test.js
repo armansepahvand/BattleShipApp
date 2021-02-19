8ec5f07aa11092c1799f12391ef294b453a94ac7
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-19 12:49:05 -0700

    added test cases to the test

--- tests/BattleShipBoard.test.js
+++ tests/BattleShipBoard.test.js
@@ -1,9 +1,11 @@
 const BattleShipBoard = require('../classes/BattleShipBoard');
 
+test('check for the slots in the board ', () => {
+  const battleShipBoard = new BattleShipBoard('e', 8);
+  expect(battleShipBoard.isInRange('b', 2)).toBe(true);
+  expect(battleShipBoard.isInRange('h', 1)).toBe(false);
+  expect(battleShipBoard.isInRange('c', 9)).toBe(false);
+  expect(battleShipBoard.isInRange('d', 8)).toBe(true);
+  expect(battleShipBoard.isInRange('1', 8)).toBe(false);
 
-
-test('check if the slot exists in the board', () => {
-    const battleShipBoard= new BattleShipBoard();
-    expect(battleShipBoard.checkSlot("a1")).toBe(true)
-    
-})
+});
