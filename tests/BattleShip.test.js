2c1f5471e2a80802b01ee5d836f901078db246f8
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-19 16:06:31 -0700

    test setters for BattleShip Class

--- tests/BattleShip.test.js
+++ tests/BattleShip.test.js
@@ -1,12 +1,16 @@
 const BattleShip = require('../classes/BattleShip');
 
 test('Vertical Ship blocks should be in sequence numerically ', () => {
-  const battleShipVertical = new BattleShip('b3', 3, 'v');
+  const battleShipVertical = new BattleShip();
+  battleShipVertical.setOrientation = 'v';
+  battleShipVertical.setNumberOfSlots = 3;
+  battleShipVertical.setStartSlot = 'b3';
+  console.log('dfsdfsdf', battleShipVertical);
+
   expect(battleShipVertical.sequenceOfSlots).toEqual('b3b4b5');
 });
 
 test('Ship blocks should be in sequence  alphabetically', () => {
   const battleShipHorizontal = new BattleShip('b3', 3, 'h');
-  console.log(battleShipHorizontal.orientation);
   expect(battleShipHorizontal.sequenceOfSlots).toEqual('b3c3d3');
 });
