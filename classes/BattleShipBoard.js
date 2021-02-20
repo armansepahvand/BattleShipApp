ccc366e4ac636dcfa35828b97e2f4f364a5b1015
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-20 14:05:37 -0700

    Changed the battleShipBoard class to accept a string as a ship

--- classes/BattleShipBoard.js
+++ classes/BattleShipBoard.js
@@ -1,23 +1,43 @@
 //A class to create the game board by asking the player to giv the dimensions of the board
 module.exports = class BattleShipBoard {
-  constructor(width, height) {
+  constructor(width = 'h', height = 8) {
     this.width = width.toLowerCase();
     this.height = height;
   }
 
+  /**
+   * @param {string} value
+   */
+  set setWidth(value) {
+    this.width = value;
+  }
+
+  /**
+   * @param {number} value
+   */
+  set setHeight(value) {
+    this.height = value;
+  }
+
   //method to check if the given letter number combination is in the range of the board
-  isInRange(testWidth, testHeight) {
+  isShipInRange(shipSlots) {
+    const arrayofSlots = shipSlots.match(/.{1,2}/g);
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    const lowerCaseWidth = testWidth.toLowerCase();
-    const testWidthIndex = alphabet.indexOf(lowerCaseWidth);
-    const widthIndex = alphabet.indexOf(this.width);
-    if (
-      testHeight <= this.height &&
-      testWidthIndex <= widthIndex &&
-      testWidthIndex >= 0
-    ) {
-      return true;
+    let state = true;
+    for (let slot of arrayofSlots) {
+      let lowerCaseWidth = slot[0].toLowerCase();
+      let testWidthIndex = alphabet.indexOf(lowerCaseWidth);
+      let widthIndex = alphabet.indexOf(this.width);
+      let testHeight = slot[1];
+      if (
+        testHeight > this.height ||
+        testWidthIndex > widthIndex ||
+        testWidthIndex < 0
+      ) {
+        state = false;
+        break;
+      }
     }
-    return false;
+    return state;
   }
 };
--- tests/BattleShipBoard.test.js
+++ tests/BattleShipBoard.test.js
@@ -2,10 +2,8 @@ const BattleShipBoard = require('../classes/BattleShipBoard');
 
 test('check for the slots in the board ', () => {
   const battleShipBoard = new BattleShipBoard('e', 8);
-  expect(battleShipBoard.isInRange('b', 2)).toBe(true);
-  expect(battleShipBoard.isInRange('h', 1)).toBe(false);
-  expect(battleShipBoard.isInRange('c', 9)).toBe(false);
-  expect(battleShipBoard.isInRange('d', 8)).toBe(true);
-  expect(battleShipBoard.isInRange('1', 8)).toBe(false);
-
+  expect(battleShipBoard.isShipInRange('b2b3b4')).toBe(true);
+  expect(battleShipBoard.isShipInRange('a2b2c2')).toBe(true);
+  expect(battleShipBoard.isShipInRange('b1b3b9')).toBe(false);
+  expect(battleShipBoard.isShipInRange('z2b2c2')).toBe(false);
 });
