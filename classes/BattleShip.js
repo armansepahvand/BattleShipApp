fd0e4e9a5d9e9b40b8ac81432ae8f9c9a851c786
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-20 14:20:22 -0700

    fixed naming of the variables and methods

--- classes/BattleShip.js
+++ classes/BattleShip.js
@@ -1,22 +1,22 @@
 //A class to create the battleship objects by asking the player to input the
 //starting slot, number of slots and orientation of the ship
 module.exports = class BattleShip {
-  constructor(shipslots = 'a1a2a3') {
-    this.shipslots = shipslots.match(/.{1,2}/g);
+  constructor(shipSlots = 'a1a2a3') {
+    this.shipSlots = shipSlots.match(/.{1,2}/g);
     this.length = 3;
   }
 
   /**
    * @param {number} value
    */
-  set setlength(value) {
+  set setLength(value) {
     this.length = value;
   }
 
   /**
    * @param {string[]} value
    */
-  set setshipslots(value) {
-    this.shipslots = value.match(/.{1,2}/g);
+  set setShipSlots(value) {
+    this.shipSlots = value.match(/.{1,2}/g);
   }
 };
