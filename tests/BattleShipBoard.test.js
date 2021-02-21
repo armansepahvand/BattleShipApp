7a99769b898d77cb8f22492e0a942212ba06c4b5
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2021-02-20 17:29:55 -0700

    Seperated  the tests

--- tests/BattleShipBoard.test.js
+++ tests/BattleShipBoard.test.js
@@ -1,9 +1,15 @@
 const BattleShipBoard = require('../classes/BattleShipBoard');
+const battleShipBoard = new BattleShipBoard('e', 8);
 
-test('check for the slots in the board ', () => {
-  const battleShipBoard = new BattleShipBoard('e', 8);
+test('Should pass for correct vertical ship in range ', () => {
   expect(battleShipBoard.isShipInRange('b2b3b4')).toBe(true);
+});
+test('Should pass for correct horizotal ship in range ', () => {
   expect(battleShipBoard.isShipInRange('a2b2c2')).toBe(true);
+});
+test('Should pass for incorrect height of ship in range  ', () => {
   expect(battleShipBoard.isShipInRange('b1b3b9')).toBe(false);
+});
+test('Should pass for incorrect width of ship in range  ', () => {
   expect(battleShipBoard.isShipInRange('z2b2c2')).toBe(false);
 });
